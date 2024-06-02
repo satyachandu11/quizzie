e3fb9ead4c1e6913d78d7e24ed557f78382330e8
satyachandu11 <rockonchandu11@gmail.com>
2024-06-02 16:54:49 +0530

    Revert "deploying"
    This reverts commit 5d27c641f63f2ab0fd590176969d164155c68cc6.

--- client/src/utils/FormatDate.jsx
+++
@@ -1,8 +0,0 @@
-function formatDate(inputDateStr) {
-  var inputDate = new Date(inputDateStr);
-  var options = { day: "numeric", month: "short", year: "numeric" };
-  var outputDateStr = inputDate.toLocaleDateString("en-US", options);
-  return outputDateStr;
-}
-
-export default formatDate;
